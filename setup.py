9e2eab7b3ef03e43456ffcd985b486a92eb201a7
Tejaswini <davutejaswini2000@gmail.com>
2025-01-09 13:16:59 +0530

    all chnages updated

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tejaswini",
+    author_email="davutejaswini2000@gmail@.com",
+    packages=find_packages()
+)
+
+
+
+
+
